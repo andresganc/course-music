9212a251350ab0e0f9e36d8bdcd3b87d4ada6617
Andres Giraldo <andresganc@gmail.com>
2022-07-01 15:08:06 -0500

    Mod: Array

--- src/main/kotlin/basic/doc/17-arrays.kt
+++ src/main/kotlin/basic/doc/17-arrays.kt
@@ -1,2 +1,10 @@
 package basic.doc
 
+fun main() {
+    weekday(2)
+}
+
+fun weekday(numArray: Int): String {
+    val arrayWeekDay = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" )
+    return arrayWeekDay[numArray]
+}
