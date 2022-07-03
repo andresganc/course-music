c62e4a479ef357bc9f666fce93664ca6646f1cba
Andres Giraldo <andresganc@gmail.com>
2022-07-03 13:50:50 -0500

    Mod: Arrays

--- src/main/kotlin/basic/doc/17-arrays.kt
+++ src/main/kotlin/basic/doc/17-arrays.kt
@@ -1,10 +1,25 @@
 package basic.doc
 
 fun main() {
-    weekday(2)
+    println(weekday(0))
+    arraySet()
+    arraySize()
 }
 
 fun weekday(numArray: Int): String {
     val arrayWeekDay = arrayOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" )
     return arrayWeekDay[numArray]
+}
+
+// Array Set
+fun arraySet() {
+    val profile = arrayOf("Jorge", "Andres", "Giraldo", "Arenas", "42")
+    profile.set(4, "43 ")
+    println(profile)
+}
+
+// Array Size
+fun arraySize() {
+    val fruits = arrayOf("Pears", "Apples", "Grapes", "Strawberries", "Cherries", "peach")
+    println(fruits.size)
 }
